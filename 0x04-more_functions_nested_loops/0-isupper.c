9f327817ca4939d4e93ed43b7914790834589a8e
andrew11212 <andrew,atef299@gmail.com>
2023-06-23 09:13:11 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,10 @@
  *
  * Return 1 if its uppercase , 0if not
  */
-int _isupper(int c);
+int _isupper (int c)
 {
-	if (c >=65 && c<= 90)
-		return(1);
+	if (c >= 65 && c <= 90)
+		return (1);
 	else
-		return(0);
+		return (0);
 }

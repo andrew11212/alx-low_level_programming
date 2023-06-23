266589ade26b65be1a0ae5ff0e0579de8b170e92
andrew11212 <andrew,atef299@gmail.com>
2023-06-23 09:19:37 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *isupper - check if c is upper
- *@c is input for alphapet
- * Return 1 if its uppercase , 0if not
+ *_isupper - check if c is upper
+ * @c: is input for alphapet
+ * Return: 1 if its uppercase , 0 if not
  */
-int _isupper (int c)
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
